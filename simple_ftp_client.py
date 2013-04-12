4784eaccc3042533e9000bb4fd9b1c7599fd3c0b
unknown <nikhilloya88@gmail.com>
2013-04-11 21:02:45 -0400

    Coded till sending message in client

--- simple_ftp_client.py
+++ simple_ftp_client.py
@@ -3,6 +3,7 @@ import threading
 
 transmitted = -1
 acked = -1
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 class myThread(threading.thread):
 	def __init__(self, sock):
@@ -19,12 +20,10 @@ def main():
 	if len(sys.argv) == 6:
 		global transmitted
 		global acked
-		host = sys.argv[1]
-		port = sys.argv[2]
-		filepath = sys.argv[3]
-		window_size = sys.argv[4]
-		mss = sys.argv[5]		
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		global s
+		global host
+		global port
+		global mss
 		s.connect((host,port))
 		t = myThread(s)
 		t.start()
@@ -32,9 +31,14 @@ def main():
 			f = open(filepath, 'r')
 			msg = ""
 			while True:
-				c = rdt_send(f)
+				rdt_send(f)
+				#c = rdt_send(f)
 				if c != '':
 					msg += c
+					if(len(msg) == mss):
+						prepare segment
+						s.send(msg)
+						msg = ""
 				else:
 					break
 		except IOError as e:
@@ -42,7 +46,43 @@ def main():
 	else:
 		print "Provide correct number of arguments. There should be 5 arguments."
 
-def rdt_send(f):
-	return f.read(1)
+def rdt_send(f):	
+	global window_size
+	global mss
+	msg = ""
+	c = f.read(1)
+	while c != '':
+		msg += c
+		if(len(msg)==mss):
+			while transmitted - acked == window_size:
+				# loop till window_size is full
+			
+			sendtoserver(msg)
+			msg = ""
+		c = f.read(1)
+		
+	if(len(msg)!=0): # if the file is read completely and the last chunk of msg is not sent as it is not 1024 then send that last chunk of msg
+		sendtoserver(msg)
 
+def sendtoserver(msg):
+	global s
+	global buffer
+	global transmitted
+	
+	segment = ""
+	transmitted += 1
+	buffer[transmitted % window_size] = msg
+	checksum = ""
+	segment += "{0:032b}".format(transmitted)
+	segment += "{0:016b}".format(checksum)
+	segment += '0101010101010101'
+	s.send(segment)
+	
+		
+host = sys.argv[1]
+port = sys.argv[2]
+filepath = sys.argv[3]
+window_size = sys.argv[4]
+mss = sys.argv[5]
+buffer = window_size*[None] # initialize buffer with size as window_size
 main()
