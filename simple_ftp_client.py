d0589c9e25516bb4de360404e284cb664f5468a2
unknown <nikhilloya88@gmail.com>
2013-04-13 17:22:39 -0400

    Done with all the functionalities except the extra credit part. Need to test thoroughly as well.

--- simple_ftp_client.py
+++ simple_ftp_client.py
@@ -2,10 +2,32 @@ import sys
 import threading
 import datetime
 import socket
+import time
 
 transmitted = -1
 acked = -1
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# REFERENCE: http://codewiki.wikispaces.com/ip_checksum.py
+def calculate_checksum(data):  # Form the standard IP-suite checksum
+  pos = len(data)
+  if (pos & 1):  # If odd...
+    pos -= 1
+    sum = ord(data[pos])  # Prime the sum with the odd end byte
+  else:
+    sum = 0
+ 
+  #Main code: loop to calculate the checksum
+  while pos > 0:
+    pos -= 2
+    sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
+ 
+  sum = (sum >> 16) + (sum & 0xffff)
+  sum += (sum >> 16)
+ 
+  result = (~ sum) & 0xffff #Keep lower 16 bits
+  result = result >> 8 | ((result & 0xff) << 8)  # Swap bytes
+  return result
 
 class myThread(threading.Thread):
 	def __init__(self, sock):
@@ -22,8 +44,12 @@ class myThread(threading.Thread):
 			data,addr = self.sock.recvfrom(64)
 			seq_no = int(data[0:32], 2)			
 			hdr = data[32:48]
-			ackpkt = data[48:64]			
-			acked = seq_no
+			ackpkt = data[48:64]	
+			'''
+			if ackpt != "1010101010101010":
+				print("Error in the acknowledge packet")
+			'''
+			acked = seq_no # update the acked field
 
 class PktSentHandler(threading.Thread):
 	def __init__(self, sock, datetimesent, seq_no, segment):
@@ -38,9 +64,10 @@ class PktSentHandler(threading.Thread):
 		global port
 		while True:
 			''' 5 being the timeout value. Wait for timeout and resend'''
-			while (datetime.datetime.now() - self.datetimesent).seconds < 5 and acked < self.seq_no:
-				pass # infinite loop			
+			while (datetime.datetime.now() - self.datetimesent).seconds < 2 and acked < self.seq_no:
+				pass # loop	till timeout occurs or packet is acknowledged		
 			if acked < self.seq_no: # this means that the while loop was broken because timeout occured, resend packet
+				print("Timeout, sequence number = %s" %self.seq_no)
 				self.sock.sendto(bytes(self.segment, 'UTF-8'), (host,port))
 				self.datetimesent = datetime.datetime.now()
 			else: # that means packet was acknowledged
@@ -55,30 +82,21 @@ def main():
 	global host
 	global port
 	global mss
-	print("Host: %s" %host)
+	print("My IP: %s" %socket.gethostbyname(socket.gethostname()))
 	s.connect((host,port))
 	#t = myThread(s)
 	#t.start()
 	try:
 		f = open(filepath, 'r')
 		msg = ""
-		rdt_send(f)
-		'''
-		while True:
-			rdt_send(f)
-			#c = rdt_send(f)
-			if c != '':
-				msg += c
-				if(len(msg) == mss):
-					prepare segment
-					s.send(msg)
-					msg = ""
-			else:
-				break
-		'''
+		rdt_send(f)		
 	except IOError as e:
 		print("File Not Found or you didn't enter path in quotes or the ordering of arguments supplied is incorrect.")
+	
+	while threading.active_count() != 2:
+		pass
 	print("End of Program")
+	#s.close()
 
 def rdt_send(f):	
 	global window_size
@@ -111,16 +129,19 @@ def sendtoserver(msg):
 	
 	segment = ""
 	transmitted += 1
-	buffer[transmitted % window_size] = msg
-	checksum = 0
+	#buffer[transmitted % window_size] = msg
+	checksum = calculate_checksum(msg)
+	
 	segment += "{0:032b}".format(transmitted)
 	segment += "{0:016b}".format(checksum)
 	segment += '0101010101010101'
 	segment += msg
-	print (segment)
+	
 	s.sendto(bytes(segment, 'UTF-8'), (host,port))
 	p = PktSentHandler(s, datetime.datetime.now(), transmitted, segment)
 	p.start()
+	#p.join()
+	#p.exit()
 
 # THESE LINES WOULD BE EXECUTED FIRST	
 if len(sys.argv) == 6:
@@ -129,7 +150,10 @@ if len(sys.argv) == 6:
 	filepath = sys.argv[3]
 	window_size = int(sys.argv[4])
 	mss = int(sys.argv[5])
-	buffer = window_size*[None] # initialize buffer with size as window_size
-	main()
+	#buffer = window_size*[None] # initialize buffer with size as window_size
+	if mss < 0 or window_size <= 0 or port != 7735:
+		print("One of mss, window_size or port value is incorrect.")
+	else:
+		main()
 else:
-	print ("Provide correct number of arguments. There should be 5 arguments, you have given %s." %len(sys.argv))	
+	print ("Provide correct number of arguments. There should be 5 arguments, you have given %s." %len(sys.argv))
--- simple_ftp_server.py
+++ simple_ftp_server.py
@@ -2,13 +2,37 @@ import sys
 import threading
 import datetime
 import socket
+import random
+
+# REFERENCE: http://codewiki.wikispaces.com/ip_checksum.py
+def calculate_checksum(data):  # Form the standard IP-suite checksum
+  pos = len(data)
+  if (pos & 1):  # If odd...
+    pos -= 1
+    sum = ord(data[pos])  # Prime the sum with the odd end byte
+  else:
+    sum = 0
+ 
+  #Main code: loop to calculate the checksum
+  while pos > 0:
+    pos -= 2
+    sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
+ 
+  sum = (sum >> 16) + (sum & 0xffff)
+  sum += (sum >> 16)
+ 
+  result = (~ sum) & 0xffff #Keep lower 16 bits
+  result = result >> 8 | ((result & 0xff) << 8)  # Swap bytes
+  return result
 
 def main():
 	global filename
 	global received
+	global p
+	global port
 	skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	host = socket.gethostbyname(socket.gethostname())
-	port = 7735
+	
 	print("Host: %s" %host)
 	skt.bind((host, port))
 	#skt.listen(5)
@@ -20,32 +44,48 @@ def main():
 		
 	while True:		
 		data, address = skt.recvfrom(1088)		
+		data = data.decode('UTF-8')
 		seq_no = int(data[0:32], 2)
-		checksum = data[32:48]
+		checksum_incoming_pkt = int(data[32:48],2)
 		hdr = data[48:64]
-		segment = data[64:]
+		segment = data[64:]		
+		checksum_value = calculate_checksum(segment)
+		#print("Checksum for %s is: %s (%s)" %(str(seq_no), str(checksum_value), data[64:]))					
+		
+		r = random.random()
+		# if the packets arrive out of order no need to do anything
 		if seq_no - received == 1:
-			f = open(filename, 'ab')
-			print(segment)
-			print("Here")
-			print(str(segment))
-			received += 1
-			f.write(segment)
-			'''this would work as this is the value that is being sent anyways. it should be received field value. data[0:32]'''
-			segment = data[0:32] 
-			segment += bytes("{0:016b}".format(0),'UTF-8')
-			segment += bytes("1010101010101010", 'UTF-8')
-			#print segment
-			skt.sendto(segment, address)
-			f.close()
+			if (r > p) and (checksum_incoming_pkt == checksum_value) and (hdr == '0101010101010101'):
+				print("Packet received, sequence number = %s" %str(seq_no))
+				f = open(filename, 'ab')				
+				received += 1
+				f.write(bytes(segment, 'UTF-8'))			
+				'''this would work as this is the value that is being sent anyways. it should be received field value. data[0:32]'''
+				segment = data[0:32]
+				segment += "{0:016b}".format(0)
+				segment += "1010101010101010"				
+				skt.sendto(bytes(segment, "UTF-8"), address)
+				f.close()
+			elif r<=p:
+				print("Packet loss, sequence number = %s" %str(seq_no))
+			elif checksum_incoming_pkt != checksum_value:
+				print("Checksum for sequence number: %s is incorrect" %str(seq_no))
+			else:
+				print("Value should be: 0101010101010101 but found something else")
 		
 
 if len(sys.argv) == 4:
 	received = -1
-	port = sys.argv[1]
+	port = int(sys.argv[1])
 	filename = sys.argv[2]
-	p = sys.argv[3]
-	main()
+	p = float(sys.argv[3])
+	if p>0 and p<1 and port==7735:
+		main()
+	elif port!=7735:
+		print("Value of port should be 7735")
+	else:
+		print("Value of p should be between 0 and 1, excluding 0 and 1.")
 	print ("End of Program")
 else:
 	print("There should be 3 command-line arguments only.")
+
