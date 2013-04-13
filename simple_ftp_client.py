011c74e4fde95a2768d06f9fae79b80aececc7a9
unknown <nikhilloya88@gmail.com>
2013-04-12 21:13:32 -0400

    Almost done except error handling, checksum and probability

--- simple_ftp_client.py
+++ simple_ftp_client.py
@@ -1,11 +1,13 @@
 import sys
 import threading
+import datetime
+import socket
 
 transmitted = -1
 acked = -1
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-class myThread(threading.thread):
+class myThread(threading.Thread):
 	def __init__(self, sock):
 		threading.Thread.__init__(self)
 		self.sock = sock
@@ -13,51 +15,87 @@ class myThread(threading.thread):
 	def run(self):
 		global transmitted
 		global acked
-		while True:
-			self.sock.recv(1024)
-			
-def main():
-	if len(sys.argv) == 6:
-		global transmitted
-		global acked
 		global s
+		#h = socket.gethostbyname(socket.gethostname())
+		#s.bind((h, 50001))
+		while True:
+			data,addr = self.sock.recvfrom(64)
+			seq_no = int(data[0:32], 2)			
+			hdr = data[32:48]
+			ackpkt = data[48:64]			
+			acked = seq_no
+
+class PktSentHandler(threading.Thread):
+	def __init__(self, sock, datetimesent, seq_no, segment):
+		threading.Thread.__init__(self)
+		self.sock = sock
+		self.datetimesent = datetimesent
+		self.seq_no = seq_no
+		self.segment = segment
+	
+	def run(self):
 		global host
 		global port
-		global mss
-		s.connect((host,port))
-		t = myThread(s)
-		t.start()
-		try:
-			f = open(filepath, 'r')
-			msg = ""
-			while True:
-				rdt_send(f)
-				#c = rdt_send(f)
-				if c != '':
-					msg += c
-					if(len(msg) == mss):
-						prepare segment
-						s.send(msg)
-						msg = ""
-				else:
-					break
-		except IOError as e:
-			print("File Not Found or you didn't enter path in quotes or the ordering of arguments supplied is incorrect.")
-	else:
-		print "Provide correct number of arguments. There should be 5 arguments."
+		while True:
+			''' 5 being the timeout value. Wait for timeout and resend'''
+			while (datetime.datetime.now() - self.datetimesent).seconds < 5 and acked < self.seq_no:
+				pass # infinite loop			
+			if acked < self.seq_no: # this means that the while loop was broken because timeout occured, resend packet
+				self.sock.sendto(self.segment, (host,port))
+				self.datetimesent = datetime.datetime.now()
+			else: # that means packet was acknowledged
+				break
+
+			
+def main():
+	
+	global transmitted
+	global acked
+	global s
+	global host
+	global port
+	global mss
+	print("Host: %s" %host)
+	s.connect((host,port))
+	#t = myThread(s)
+	#t.start()
+	try:
+		f = open(filepath, 'r')
+		msg = ""
+		rdt_send(f)
+		'''
+		while True:
+			rdt_send(f)
+			#c = rdt_send(f)
+			if c != '':
+				msg += c
+				if(len(msg) == mss):
+					prepare segment
+					s.send(msg)
+					msg = ""
+			else:
+				break
+		'''
+	except IOError as e:
+		print("File Not Found or you didn't enter path in quotes or the ordering of arguments supplied is incorrect.")
+	print "End of Program"
 
 def rdt_send(f):	
 	global window_size
 	global mss
 	msg = ""
+	flag = True
 	c = f.read(1)
-	while c != '':
+	while c != '': # EOF
 		msg += c
 		if(len(msg)==mss):
 			while transmitted - acked == window_size:
-				# loop till window_size is full
-			
+				pass # loop till window_size is full
 			sendtoserver(msg)
+			if flag:
+				flag = False
+				t = myThread(s)
+				t.start()
 			msg = ""
 		c = f.read(1)
 		
@@ -68,21 +106,30 @@ def sendtoserver(msg):
 	global s
 	global buffer
 	global transmitted
+	global host
+	global port
 	
 	segment = ""
 	transmitted += 1
 	buffer[transmitted % window_size] = msg
-	checksum = ""
+	checksum = 0
 	segment += "{0:032b}".format(transmitted)
 	segment += "{0:016b}".format(checksum)
 	segment += '0101010101010101'
-	s.send(segment)
-	
-		
-host = sys.argv[1]
-port = sys.argv[2]
-filepath = sys.argv[3]
-window_size = sys.argv[4]
-mss = sys.argv[5]
-buffer = window_size*[None] # initialize buffer with size as window_size
-main()
+	segment += msg
+	#print segment
+	s.sendto(segment, (host,port))
+	p = PktSentHandler(s, datetime.datetime.now(), transmitted, segment)
+	p.start()
+
+# THESE LINES WOULD BE EXECUTED FIRST	
+if len(sys.argv) == 6:
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+	filepath = sys.argv[3]
+	window_size = int(sys.argv[4])
+	mss = int(sys.argv[5])
+	buffer = window_size*[None] # initialize buffer with size as window_size
+	main()
+else:
+	print "Provide correct number of arguments. There should be 5 arguments, you have given %s." %len(sys.argv)	
---
+++ simple_ftp_server.py
@@ -0,0 +1,46 @@
+import sys
+import threading
+import datetime
+import socket
+
+def main():
+	global filename
+	global received
+	skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	host = socket.gethostbyname(socket.gethostname())
+	port = 7735
+	print("Host: %s" %host)
+	skt.bind((host, port))
+	#skt.listen(5)
+	#client,addr = skt.accept()
+	try:
+		f = open(filename, 'w')		
+	except IOError as e:
+		print("File Not Found or you didn't enter correct path.")	
+		
+	while True:		
+		data, address = skt.recvfrom(1088)
+		seq_no = int(data[0:32], 2)
+		checksum = data[32:48]
+		hdr = data[48:64]
+		segment = data[64:]
+		if seq_no - received == 1:
+			received += 1
+			f.write(segment)
+			'''this would work as this is the value that is being sent anyways. it should be received field value. data[0:32]'''
+			segment = data[0:32] 
+			segment += "{0:016b}".format(0)
+			segment += "1010101010101010"
+			#print segment
+			skt.sendto(segment, address)
+		
+
+if len(sys.argv) == 4:
+	received = -1
+	port = sys.argv[1]
+	filename = sys.argv[2]
+	p = sys.argv[3]
+	main()
+	print "End of Program"
+else:
+	print("There should be 3 command-line arguments only.")
